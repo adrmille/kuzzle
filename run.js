08acb3ca9ca9d40c2da419c46aab44ec67cd8e1d
David Bengsch <dbengsch@kaliop.com>
2017-02-10 17:53:54 +0100

    WIP

--- run.js
+++ run.js
@@ -1,9 +1,9 @@
 const
   Kuzzle = require('./index'),
   kuzzle = new Kuzzle(__dirname, {}),
-  authLocalPlugin = require('plugins/enabled/kuzzle-plugin-auth-passport-local');
+  authLocalPlugin = require('./plugins/enabled/kuzzle-plugin-auth-passport-local');
 
 kuzzle
   .registerPlugin('kuzzle-plugin-auth-passport-local', authLocalPlugin, {})
-  .registerThreadPlugin('kuzzle-plugin-logger', 'plugins/enabled/kuzzle-plugin-logger', {threads: 1})
+  .registerThreadPlugin('kuzzle-plugin-logger', 'plugins/enabled/kuzzle-plugin-logger', {}, 1)
   .start();
